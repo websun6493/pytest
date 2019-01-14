f75a2ab240b06c8103db9f7757c81658d3ab29dc
websun6493 <websun6493@gmail.com>
2019-01-14 21:27:09 +0800

    1/14

--- proj-1.py
+++ proj-1.py
@@ -1,3 +1,13 @@
-print("hello world")
-print("hello again")
-print("byebye")
+x = input('what do you want: ')
+y = int(input('first number: '))
+z = int(input('second number: '))
+if x==('+'):
+	print(y+z)
+elif x ==('-'):
+	print(y-z)
+elif x ==('*'):
+	print(y*z)
+elif x ==('/'):
+	print(y/z)
+else:
+	print('wrong!')
