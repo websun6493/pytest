4eb72746c10205b002a7afeacea3979f457a157f
websun6493 <websun6493@gmail.com>
2019-02-02 22:12:08 +0800

    ttt

---
+++ ff.py
@@ -0,0 +1,60 @@
+def area (width, heigh):
+	return width * heigh
+def print_welcome (name):
+	print("KIO", name)
+print("hello, what is your name?")
+name = input("")
+name.print_welcome
+i = ['3','2','1']
+for num in i:
+	print(num)
+while len(i)==3:
+	print("enter a number:")
+	a = input()
+	print("enter again:")
+	c = input()
+	if a == c:
+		print("you are honest\n" + '-'*15)
+	else:
+		print("you are a liyer!")
+		print("game over!\n" + '-'*15)
+		i.append('0')
+print("test_code\n" + '-'*15)
+print("coder:yi\n" + '-'*15)
+print("a or inputb?")
+c = input()
+if c == 'a':
+	i = 0
+	while i<1:
+		print("a?(y or n)")
+		x = raw_input('')
+		if (x is y):
+			print("ok,good")
+			i += 1
+		elif (x is n):
+			print("ok,then choose again!")
+elif (c is b):
+	p = 0
+	while p<1:
+		print("b?(y or n)")
+		y = input("")
+		if p == y:
+			print("ok,good")
+			p += 1
+		elif p == n:
+			print("ok then choose again!")
+elif c == c:
+	l = 0
+	while l<1:
+		print("c?(y or n)")
+		z = input("")
+		if l == y:
+			print("ok,good")
+			l += 1
+		elif l == n:
+			print("ok then choose again!")
+else:
+	print("wrong!")
+str(c)
+o = {"a":'apple',"b":'ball',"c":'cat'}
+print(o[ 'c' ])
---
+++ uu.py
@@ -0,0 +1,12 @@
+print("hello")
+i = 11
+a = 1
+b = 1
+l = []
+while i > 0:
+	a += b
+	l.append(a)
+	i -= 1
+for show in l:
+	print(show)
+	
